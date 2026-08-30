ef586fc7ca7f86ecde38d2bde099899ef492ff06
Wanyin <3114751386@qq.com>
2026-08-30 19:51:58 +0800

    refactor(repo): 彻底清除毕设文献阅读器临时文件，独立重构迁移至 Graduation Project 专项目录

--- 题库/毕设/generate_reader_papers.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Master Paper Generator for Bishe
-import os, sys
-scratch_dir = os.path.join(os.path.expanduser('~'), r'.geminintigravityrain9ecdc11-aa6d-4b48-a97a-f9a15ea6e3c5\scratch')
-sys.path.append(scratch_dir)
-import make_paper1_full
-import make_paper2_full
-import make_paper3_full
-print('All 3 papers successfully generated!')
